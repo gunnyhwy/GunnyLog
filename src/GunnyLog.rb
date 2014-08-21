5cb2cd4fabe2bcfe2a63d091fdfadbbeaa716b2b
Keith Kowalski <keithk23@gmail.com>
2014-08-21 16:12:02 -0500

    next commit

--- src/GunnyLog.rb
+++ src/GunnyLog.rb
@@ -10,46 +10,46 @@ class GunnyLog
     @@onoffswitch = true
     @@location = "MainMethod"
         
-    def setSwitch(switch)
+    def setSwitch(switch) # switch logging on and off
         @@onoffswitch = switch
     end
 
-    def setLocation(name)
+    def setLocation(name) # set message location
         @@location = name
     end
     
-    def message(loc = nil, msg)
+    def message(loc = nil, msg) # write message
             write_msg(STDOUT, loc, msg)
     end  
     
     private
-    
-    def date_str
-        d = DateTime.now
-        d.strftime("%m/%d/%Y|%I:%M:%S%p")
-    end
-    
-    def write_msg(output, loc, msg)
+
+    def write_msg(output, loc, msg)  
         if loc == nil then loc = @@location end
         if @@onoffswitch
             output.puts "#{date_str}|#{loc}|#{msg}"
         end        
     end
         
+    def date_str
+        d = DateTime.now
+        d.strftime("%m/%d/%Y|%I:%M:%S%p")
+    end
+    
 end    
 
 # GunnyLogFile logs messages to a file
 class GunnyLogFile < GunnyLog
     
-    def open(filename = "gunnylog")
+    def open(filename = "gunnylog") # open the file
         @outfile = File.open("#{filename}.log", "a+")
     end
         
-    def close
+    def close # close the file
     	@outfile.close
     end         
 
-    def message(loc = nil, msg)
+    def message(loc = nil, msg) # write message to file
         write_msg(@outfile, loc, msg)
     end
  
