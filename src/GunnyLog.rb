f11093d04263407f26d73c851df43e57ffd75aa8
Keith Kowalski <keithk23@gmail.com>
2014-08-21 15:20:04 -0500

    next commit

--- src/GunnyLog.rb
+++ src/GunnyLog.rb
@@ -1,31 +1,54 @@
-# Ruby logfile class
-require "date"
+require 'singleton'
+require 'date'
 
+# GunnyLog logs messages to stdout
 class GunnyLog
     
-    def initialize(appname, classname = appname, onoffswitch = true)
-        @appname = appname
-        @classname = classname
-        @onoffswitch = onoffswitch
-        @outfile = File.open("#{@appname}.log", "a+")
-    end
+    include Singleton
+    @@onoffswitch = true
+    @@location = "MainMethod"
         
-    def message(msg)
-        if @onoffswitch
-            @outfile.puts "#{date_str}|#{@classname}|#{msg}"
-        end
+    def setSwitch(switch)
+        @@onoffswitch = switch
     end
 
-    def close
-        @outfile.close
+    def setLocation(name)
+        @@location = name
     end
-
+    
+    def message(loc = nil, msg)
+            write_msg(STDOUT, loc, msg)
+    end  
+    
     private
+    
     def date_str
         d = DateTime.now
-        d.strftime("%m/%d/%Y|%I:%M%p")
+        d.strftime("%m/%d/%Y|%I:%M:%S%p")
     end
+    
+    def write_msg(output, loc, msg)
+        if loc == nil then loc = @@location end
+        if @@onoffswitch
+            output.puts "#{date_str}|#{loc}|#{msg}"
+        end        
+    end
+        
+end    
 
+#GunnyLogFile logs messages to a file
+class GunnyLogFile < GunnyLog
+    
+    def open(filename = "gunnylog")
+        @outfile = File.open("#{filename}.log", "a+")
+    end
+        
+    def close
+    	@outfile.close
+    end         
+
+    def message(loc = nil, msg)
+        write_msg(@outfile, loc, msg)
+    end
+ 
 end
-
-
--- src/testapp01.rb
+++ src/testapp01.rb
@@ -1,12 +1,35 @@
 # Test program for ruby logfile class
 require_relative 'GunnyLog'
 
-#log = ABCLog.new('testapp01','main_method', true)
-#log = ABCLog.new('testapp01','main_method')
-log = GunnyLog.new('testapp01')
 
-log.message("Log message 1")
-log.message("Log message 2")
-log.message("Log message 3")
+# Test for screen logging
+logs = GunnyLog.instance
 
-log.close
+logs.message("Testing GunnyScreenLog 01")
+logs.setSwitch(false)
+logs.message("Testing GunnyScreenLog 02")
+logs.setSwitch(true)
+logs.message("Testing GunnyScreenLog 03")
+
+logs.message("init","Testing GunnyScreenLog 04")
+logs.message("read","Testing GunnyScreenLog 05")
+logs.message("close","Testing GunnyScreenLog 06")
+
+
+#Test for file logging
+logf = GunnyLogFile.instance
+
+logf.open("zlog")
+
+logf.message("Testing GunnyFileLog 01")
+logf.setSwitch(false)
+logf.message("Testing GunnyFileLog 02")
+logf.setSwitch(true)
+logf.message("Testing GunnyFileLog 03")
+
+logf.message("init","Testing GunnyFileLog 04")
+logf.message("read","Testing GunnyFileLog 05")
+logf.message("close","Testing GunnyFileLog 06")
+
+logf.close
+
