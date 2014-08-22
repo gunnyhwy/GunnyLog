75666102ee6de10c7442ffadeb85888d4e253fca
Keith Kowalski <keithk23@gmail.com>
2014-08-21 21:58:31 -0500

    next commit

--- src/GunnyLog.rb
+++ src/GunnyLog.rb
@@ -8,43 +8,58 @@ require 'date'
 class GunnyLog
     
     include Singleton
-    @@onoffswitch = true
-    @@location = "MainMethod"
-        
-    def setSwitch(switch) # switch logging on and off
-        @@onoffswitch = switch
+
+    class << self;
+      attr_accessor :onoffswitch
+    end
+    @onoffswitch = true
+
+    class << self;
+      attr_accessor :location
+    end
+    @location = 'MainMethod'
+
+
+    # @param [bool] switch
+    def set_switch(switch) # switch logging on and off
+        @onoffswitch = switch
     end
 
-    def setLocation(name) # set message location
-        @@location = name
+    # @param [string] name
+    def set_location(name) # set message location
+        @location = name
     end
-        
+
+    # @param [string] loc
+    # @param [string] msg
     def message(loc = nil, msg) # write message
         write_msg(STDOUT, loc, msg)
-    end  
-    
-    def fmessage(loc = nil, msg, args)  # write formatted message
-        formatted = sprintf(msg, *args) # single arg or array of args 
-        message(loc, formatted) 
     end
-    
-    def fmessage_vars(loc, msg, *fargs)  # write formatted message
-        formatted = sprintf(msg, *fargs) # variable number of args 
-        message(loc, formatted) 
+
+    def formatted_message(loc = nil, msg, args)  # write formatted message
+        formatted = sprintf(msg, *args) # single arg or array of args
+        message(loc, formatted)
     end
-    
+
+    def formatted_message_vars(loc, msg, *args)  # write formatted message
+        formatted = sprintf(msg, *args) # variable number of args
+        message(loc, formatted)
+    end
+
     private
 
-    def write_msg(output, loc, msg)  
-        if loc == nil then loc = @@location end
-        if @@onoffswitch
+    def write_msg(output, loc, msg)
+        if loc == nil
+          loc = @location
+        end
+        if @onoffswitch
             output.puts "#{date_str}|#{loc}|#{msg}"
-        end        
+        end
     end
-        
+
     def date_str
         d = DateTime.now
-        d.strftime("%m/%d/%Y|%I:%M:%S%p")
+        d.strftime('%m/%d/%Y|%I:%M:%S%p')
     end
     
 end    
@@ -52,17 +67,26 @@ end
 
 # GunnyLogFile logs messages to a file
 class GunnyLogFile < GunnyLog
-    
-    def open(filename = "gunnylog") # open the file
-        @outfile = File.open("#{filename}.log", "a+")
+
+    class << self;
+      attr_accessor :_is_file_open
+    end
+    @_is_file_open = false
+
+    def open(filename = 'gunnylog') # open the file
+        @outfile = File.open(filename  + '.log', 'a+')
+        @_is_file_open = true
     end
         
     def close # close the file
     	@outfile.close
+      @_is_file_open = false
     end         
 
     def message(loc = nil, msg) # write message to file
+      if @_is_file_open
         write_msg(@outfile, loc, msg)
+      end
     end
  
 end
--- src/testapp01.rb
+++ src/testapp01.rb
@@ -3,70 +3,72 @@ require_relative 'GunnyLog'
 
 
 # Test for screen logging, one liners
-GunnyLog.instance.message("Error Catcher", "Testing again") 
-GunnyLog.instance.fmessage("Error Catcher", "Testing again = %s", "Gunny") 
-GunnyLog.instance.fmessage("Error Catcher", "Testing number = %d", 45) 
-GunnyLog.instance.fmessage("name: %s, age: %d, job: %s", ["Gunny", 45, "Developer"])
-GunnyLog.instance.fmessage("name: %s", "Gunny")
-GunnyLog.instance.fmessage_vars(nil, "name: %s, age: %d, job: %s", "Gunny", 45, "Developer")
-GunnyLog.instance.fmessage_vars("Main", "name: %s", "Gunny")
+GunnyLog.instance.message('Error Catcher', 'Testing again') 
+GunnyLog.instance.formatted_message('Error Catcher', 'Testing again = %s', 'Gunny')
+GunnyLog.instance.formatted_message('Error Catcher', 'Testing number = %d', 45)
+GunnyLog.instance.formatted_message('name: %s, age: %d, job: %s', ['Gunny', 45, 'Developer'])
+GunnyLog.instance.formatted_message('name: %s', 'Gunny')
+GunnyLog.instance.formatted_message_vars(nil, 'name: %s, age: %d, job: %s', 'Gunny', 45, 'Developer')
+GunnyLog.instance.formatted_message_vars('Main', 'name: %s', 'Gunny')
 
 
 # Test for screen logging
 logs = GunnyLog.instance
 
-logs.message("Testing GunnyScreenLog 01")
-logs.setSwitch(false)
-logs.message("Testing GunnyScreenLog 02")
-logs.setSwitch(true)
-logs.message("Testing GunnyScreenLog 03")
+logs.message('Testing GunnyScreenLog 01')
+logs.set_switch(false)
+logs.message('Testing GunnyScreenLog 02')
+logs.set_switch(true)
+logs.message('Testing GunnyScreenLog 03')
 
-logs.setLocation("init")
-logs.message("Testing GunnyScreenLog 04")
-logs.setLocation("something")
-logs.message("Testing GunnyScreenLog 05")
-logs.setLocation("destroy")
-logs.message("Testing GunnyScreenLog 06")
+logs.set_location('init')
+logs.message('Testing GunnyScreenLog 04')
+logs.set_location('something')
+logs.message('Testing GunnyScreenLog 05')
+logs.set_location('destroy')
+logs.message('Testing GunnyScreenLog 06')
 
-logs.message("init","Testing GunnyScreenLog 07")
-logs.message("read","Testing GunnyScreenLog 08")
-logs.message("close","Testing GunnyScreenLog 09")
+logs.message('init','Testing GunnyScreenLog 07')
+logs.message('read','Testing GunnyScreenLog 08')
+logs.message('close','Testing GunnyScreenLog 09')
 
-logs.fmessage("Error Catcher", "Testing again = %s", "Gunny") 
-logs.fmessage("Error Catcher", "Testing number = %d", 45) 
-logs.fmessage("name: %s, age: %d, job: %s", ["Gunny", 45, "Developer"])
+logs.formatted_message('Error Catcher', 'Testing again = %s', 'Gunny')
+logs.formatted_message('Error Catcher', 'Testing number = %d', 45)
+logs.formatted_message('name: %s, age: %d, job: %s', ['Gunny', 45, 'Developer'])
 
-logs.fmessage_vars(nil, "name: %s, age: %d, job: %s", "Gunny", 45, "Developer")
-logs.fmessage_vars("Main", "name: %s", "Gunny")
+logs.formatted_message_vars(nil, 'name: %s, age: %d, job: %s', 'Gunny', 45, 'Developer')
+logs.formatted_message_vars('Main', 'name: %s', 'Gunny')
 
 
 #Test for file logging
 logf = GunnyLogFile.instance
 
-logf.open("zlog")
+logf.open('zlog')
 
-logf.message("Testing GunnyFileLog 01")
-logf.setSwitch(false)
-logf.message("Testing GunnyFileLog 02")
-logf.setSwitch(true)
-logf.message("Testing GunnyFileLog 03")
+logf.message('Testing GunnyFileLog 01')
+logf.set_switch(false)
+logf.message('Testing GunnyFileLog 02')
+logf.set_switch(true)
+logf.message('Testing GunnyFileLog 03')
 
-logf.setLocation("init")
-logf.message("Testing GunnyFileLog 04")
-logf.setLocation("something")
-logf.message("Testing GunnyFileLog 05")
-logf.setLocation("destroy")
-logf.message("Testing GunnyFileLog 06")
+logf.set_location('init')
+logf.message('Testing GunnyFileLog 04')
+logf.set_location('something')
+logf.message('Testing GunnyFileLog 05')
+logf.set_location('destroy')
+logf.message('Testing GunnyFileLog 06')
 
-logf.message("init","Testing GunnyFileLog 07")
-logf.message("read","Testing GunnyFileLog 08")
-logf.message("close","Testing GunnyFileLog 09")
+logf.message('init','Testing GunnyFileLog 07')
+logf.message('read','Testing GunnyFileLog 08')
+logf.message('close','Testing GunnyFileLog 09')
 
-logf.fmessage("Error Catcher", "Testing again = %s", "Gunny") 
-logf.fmessage("Error Catcher", "Testing number = %d", 45) 
-logf.fmessage("name: %s, age: %d, job: %s", ["Gunny", 45, "Developer"])
+logf.formatted_message('Error Catcher', 'Testing again = %s', 'Gunny')
+logf.formatted_message('Error Catcher', 'Testing number = %d', 45)
+logf.formatted_message('name: %s, age: %d, job: %s', ['Gunny', 45, 'Developer'])
 
-logf.fmessage_vars(nil, "name: %s, age: %d, job: %s", "Gunny", 45, "Developer")
-logf.fmessage_vars("Main", "name: %s", "Gunny")
+logf.formatted_message_vars(nil, 'name: %s, age: %d, job: %s', 'Gunny', 45, 'Developer')
+logf.formatted_message_vars('Main', 'name: %s', 'Gunny')
 
 logf.close
+
+logf.message('Error Time!')
