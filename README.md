1bb1ef77c334737a7a085d1e44861b6dd498d87c
Keith Kowalski <keithk23@gmail.com>
2014-09-11 15:37:17 -0500

    Version 1.1.1

--- README.md
+++ README.md
@@ -20,7 +20,64 @@ Or install it yourself as:
 
 ## Usage
 
-See test/testapp01.rb for examples
+### Require GunnyLog
+require 'GunnyLog'
+
+### Test for screen logging, one liners
+GunnyLog.instance.message('Main:Runloop', 'Error loading preferences')<br> 
+GunnyLog.instance.message_formatted('TestClass:GetUrl', 'Error number = %d', 40)<br>
+
+### Test for screen logging
+logs = GunnyLog.instance<br>
+
+logs.message('Testing GunnyScreenLog 01')<br>
+logs.set_logging_enabled(false)<br>
+logs.message('Testing GunnyScreenLog 02 OFF')<br>
+logs.set_logging_enabled(true)<br>
+
+logs.set_message_location('init')<br>
+logs.message('Testing GunnyScreenLog 01')<br>
+logs.set_message_location('destroy')<br>
+logs.message('Testing GunnyScreenLog 02')<br>
+
+logs.message('read','Testing GunnyScreenLog 02')<br>
+logs.message('sort','Testing GunnyScreenLog 03')<br>
+
+logs.message_formatted('name: %s, age: %d, job: %s', ['Gunny', 42, 'Developer'])<br>
+logs.message_formatted_vars(nil, 'name: %s, age: %d, job: %s', 'Gunny', 42, 'Developer')<br>
+
+### Test for file logging
+logf = GunnyLogFile.instance<br>
+logf.open('testapp.log')<br>
+
+logf.message('Testing GunnyFileLog 01')<br>
+logf.set_logging_enabled(false)<br>
+logf.message('Testing GunnyFileLog 02 OFF')<br>
+logf.set_logging_enabled(true)<br>
+
+logf.set_message_location('init')<br>
+logf.message('Testing GunnyFileLog 01')<br>
+logf.set_message_location('destroy')<br>
+logf.message('Testing GunnyFileLog 02')<br>
+
+logf.message('read','Testing GunnyFileLog 02')<br>
+logf.message('sort','Testing GunnyFileLog 03')<br>
+
+logf.message_formatted('name: %s, age: %d, job: %s', ['Gunny', 42, 'Developer'])<br>
+logf.message_formatted_vars(nil, 'name: %s, age: %d, job: %s', 'Gunny', 42, 'Developer')<br>
+
+logf.close<br>
+
+### Sample output
+09/11/2014|03:33:58PM|testapp01.rb|Main:Runloop|Error loading preferences<br>
+09/11/2014|03:33:58PM|testapp01.rb|TestClass:GetUrl|Error number = 40<br>
+09/11/2014|03:33:58PM|testapp01.rb|TestClass:GetUrl|Testing GunnyScreenLog 01<br>
+09/11/2014|03:33:58PM|testapp01.rb|init|Testing GunnyScreenLog 01<br>
+09/11/2014|03:33:58PM|testapp01.rb|destroy|Testing GunnyScreenLog 02<br>
+09/11/2014|03:33:58PM|testapp01.rb|read|Testing GunnyScreenLog 02<br>
+09/11/2014|03:33:58PM|testapp01.rb|sort|Testing GunnyScreenLog 03<br>
+09/11/2014|03:33:58PM|testapp01.rb|sort|name: Gunny, age: 42, job: Developer<br>
+09/11/2014|03:33:58PM|testapp01.rb|sort|name: Gunny, age: 42, job: Developer<br>
 
 ## Documentation
 
--- test/testapp01.rb
+++ test/testapp01.rb
@@ -1,126 +1,50 @@
 # Test program for ruby logfile class
 require 'GunnyLog'
 
-puts GunnyLog::VERSION
-#log = GunnyLog.new()
-
 
 # Test for screen logging, one liners
-GunnyLog.instance.message('Error Catcher', 'Testing again') 
-GunnyLog.instance.message_formatted('Error Catcher', 'Testing again = %s', 'Gunny')
-GunnyLog.instance.message_formatted('Error Catcher', 'Testing number = %d', 42)
-GunnyLog.instance.message_formatted('name: %s, age: %d, job: %s', ['Gunny', 42, 'Developer'])
-
-GunnyLog.instance.set_output_stderr
-GunnyLog.instance.message_formatted('name: %s', 'Gunny')
-GunnyLog.instance.message_formatted_vars(nil, 'name: %s, age: %d, job: %s', 'Gunny', 42, 'Developer')
-GunnyLog.instance.message_formatted_vars('Main', 'name: %s', 'Gunny')
-GunnyLog.instance.set_output_stdout
-GunnyLog.instance.msg('Testing new method')
-
-#puts 'Logging check: ' + GunnyLog.instance.logging_enabled
-puts 'Message location: ' + GunnyLog.instance.message_location
+GunnyLog.instance.message('Main:Runloop', 'Error loading preferences')
+GunnyLog.instance.message_formatted('TestClass:GetUrl', 'Error number = %d', 40)
 
 
 # Test for screen logging
 logs = GunnyLog.instance
 
-logs.msg('Testing new method')
 logs.message('Testing GunnyScreenLog 01')
-#logs.set_switch(false)
 logs.set_logging_enabled(false)
-logs.message('Testing GunnyScreenLog 02 XXXXXXXXXXXX')
+logs.message('Testing GunnyScreenLog 02 OFF')
 logs.set_logging_enabled(true)
-#logs.set_switch(true)
-logs.message('Testing GunnyScreenLog 03')
 
-logs.set_location('init')
-logs.message('Testing GunnyScreenLog 04')
-logs.set_location('something')
-logs.message('Testing GunnyScreenLog 05')
-logs.set_message_location('destroy XXXXXXXXX')
-logs.message('Testing GunnyScreenLog 06')
+logs.set_message_location('init')
+logs.message('Testing GunnyScreenLog 01')
+logs.set_message_location('destroy')
+logs.message('Testing GunnyScreenLog 02')
 
-logs.message('init','Testing GunnyScreenLog 07')
-logs.message('read','Testing GunnyScreenLog 08')
-logs.message('close','Testing GunnyScreenLog 09')
+logs.message('read','Testing GunnyScreenLog 02')
+logs.message('sort','Testing GunnyScreenLog 03')
 
-logs.message_formatted('Error Catcher', 'Testing again = %s', 'Gunny')
-logs.message_formatted('Error Catcher', 'Testing number = %d', 42)
 logs.message_formatted('name: %s, age: %d, job: %s', ['Gunny', 42, 'Developer'])
-
 logs.message_formatted_vars(nil, 'name: %s, age: %d, job: %s', 'Gunny', 42, 'Developer')
-logs.message_formatted_vars('Main', 'name: %s', 'Gunny')
-
-#puts 'Logging check: ' + logs.logging_enabled
-puts 'Message location: ' + logs.message_location
 
 
 #Test for file logging
 logf = GunnyLogFile.instance
-
-logf.open('zlog1.log')
-
-logf.message('Testing GunnyFileLog 01')
-logf.set_switch(false)
-logf.message('Testing GunnyFileLog 02')
-logf.set_switch(true)
-logf.message('Testing GunnyFileLog 03')
-
-logf.set_location('init')
-logf.message('Testing GunnyFileLog 04')
-logf.set_location('something')
-logf.message('Testing GunnyFileLog 05')
-logf.set_location('destroy')
-logf.message('Testing GunnyFileLog 06')
-
-logf.message('init','Testing GunnyFileLog 07')
-logf.message('read','Testing GunnyFileLog 08')
-logf.message('close','Testing GunnyFileLog 09')
-
-logf.message_formatted('Error Catcher', 'Testing again = %s', 'Gunny')
-logf.message_formatted('Error Catcher', 'Testing number = %d', 42)
-logf.message_formatted('name: %s, age: %d, job: %s', ['Gunny', 42, 'Developer'])
-
-logf.message_formatted_vars(nil, 'name: %s, age: %d, job: %s', 'Gunny', 42, 'Developer')
-logf.message_formatted_vars('Main', 'name: %s', 'Gunny')
-
-logf.msg('Testing new method')
-logf.close
-
-logf.message('Error Time!')
-
-
-#Test for file logging
-logf = GunnyLog.instance
-#logf.open('zlog2.log')
-#logf.open_with_info(nil, 'zlog2', 'log')
-#logf.open_with_info('testing', 'zlog2', 'log')
-logf.open_file('logs', 'zlog2', 'log')
+logf.open('testapp.log')
 
 logf.message('Testing GunnyFileLog 01')
-logf.set_switch(false)
+logf.set_logging_enabled(false)
+logf.message('Testing GunnyFileLog 02 OFF')
+logf.set_logging_enabled(true)
+
+logf.set_message_location('init')
+logf.message('Testing GunnyFileLog 01')
+logf.set_message_location('destroy')
 logf.message('Testing GunnyFileLog 02')
-logf.set_switch(true)
-logf.message('Testing GunnyFileLog 03')
 
-logf.set_location('init')
-logf.message('Testing GunnyFileLog 04')
-logf.set_location('something')
-logf.message('Testing GunnyFileLog 05')
-logf.set_location('destroy')
-logf.message('Testing GunnyFileLog 06')
+logf.message('read','Testing GunnyFileLog 02')
+logf.message('sort','Testing GunnyFileLog 03')
 
-logf.message('init','Testing GunnyFileLog 07')
-logf.message('read','Testing GunnyFileLog 08')
-logf.message('close','Testing GunnyFileLog 09')
-
-logf.message_formatted('Error Catcher', 'Testing again = %s', 'Gunny')
-logf.message_formatted('Error Catcher', 'Testing number = %d', 42)
 logf.message_formatted('name: %s, age: %d, job: %s', ['Gunny', 42, 'Developer'])
-
 logf.message_formatted_vars(nil, 'name: %s, age: %d, job: %s', 'Gunny', 42, 'Developer')
-logf.message_formatted_vars('Main', 'name: %s', 'Gunny')
 
-logf.msg('Testing new method')
 logf.close
